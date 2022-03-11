95dbd4ab63efe29259b14f4705a20e5af7668c67
Muhammad Dhea Farizka <farizkadhea@gmail.com>
2022-03-11 11:19:36 +0700

    display users

--- blocks/testblock/block_testblock.php
+++ blocks/testblock/block_testblock.php
@@ -28,12 +28,21 @@ class block_testblock extends block_base {
     }
 
     function get_content() {
+        global $DB;
+
         if ($this->content !== NULL) {
             return $this->content;
         }
 
+        $users = $DB->get_records('user');
+
+        $userstring = '';
+        foreach($users as $user) {
+            $userstring .= $user->firstname . ' ' . $user->lastname . '<br>';
+        }
+
         $this->content = new stdClass;
-        $this->content->text = 'this is the text';
+        $this->content->text = $userstring;
         $this->content->footer = 'this is the footer';
 
         return $this->content;
