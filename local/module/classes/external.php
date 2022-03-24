d4db1797413bca14c42438d55b8a0d9a382a1341
Muhammad Dhea Farizka <farizkadhea@gmail.com>
2022-03-24 08:22:33 +0700

    Local Module Page on progress

--- local/module/classes/external.php
+++ local/module/classes/external.php
@@ -26,8 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+//require_once("../../../config.php");
 require_once($CFG->libdir . '/externallib.php');
+require_once($CFG->libdir.'/filelib.php');
 require_once($CFG->dirroot . '/mod/page/locallib.php');
+require_once($CFG->dirroot . '/course/modlib.php');
 
 /**
  * Quiz external functions
@@ -53,13 +56,116 @@ class local_module_external extends external_api {
             )
         );
     }
+
+
+
     public static function create_page_module($parameter) {
+        global $DB, $USER,$CFG;
+
+        # hardcode parameter
+        $courseid = 2;
+        $module = 'page';
+        $section = 2;
+
+
+
         $params = self::validate_parameters(self::create_page_module_parameters(),
             array(
-                'parameter' => $param_text,
+                'parameter' => $parameter,
             ));
 
-        return ['value'=> 'TEST RETURN VALUE'];
+        /*TEST AREA START*/
+
+        $course = $DB->get_record('course', array('id'=>$courseid), '*', MUST_EXIST);
+        $courseformat = course_get_format($course);
+        $maxsections = $courseformat->get_max_sections();
+
+        # Apakah section sesuai
+//        if ($section > $maxsections) {
+//            throw new \core\session\exception("INVALID_SECTION");
+//        }
+
+        list($module, $context, $cw, $cm, $data) = prepare_new_moduleinfo_data($course, $module, $section);
+
+        $sectionname = get_section_name($course, $cw);
+        $fullmodulename = get_string('modulename', $module->name);
+
+        $pageheading = get_string('addinganew', 'moodle', $fullmodulename);
+
+        $navbaraddition = $pageheading;
+
+        $pagepath = 'mod-' . $module->name . '-';
+        $pagepath .= 'mod';
+
+        $modmoodleform = "$CFG->dirroot/mod/$module->name/mod_form.php";
+        if (file_exists($modmoodleform)) {
+            require_once($modmoodleform);
+        } else {
+            print_error('noformdesc');
+        }
+
+        $mformclassname = 'mod_'.$module->name.'_mod_form';
+        $mform = new $mformclassname($data, $cw->section, $cm, $course);
+        $mform->set_data($data);
+
+        /* Mulai Input Data */
+
+        /* Coba buat POST Hardcode */
+//        $_POST = [
+//            'display' => "5",
+//            'completionunlocked' => "1",
+//            'course' => "2",
+//            'coursemodule' => "",
+//            'section' => "2",
+//            'module' => "16",
+//            'modulename' => "page",
+//            'instance' => "",
+//            'add' => "page",
+//            'update' => "0",
+//            'return' => "0",
+//            'sr' => "0",
+//            'revision' => "1",
+//            'sesskey' => "epGJOwerq5",
+//            '_qf__mod_page_mod_form' => "1",
+//            'mform_isexpanded_id_general' => "1",
+//            'mform_isexpanded_id_contentsection' => "1",
+//            'mform_isexpanded_id_appearancehdr' => "0",
+//            'mform_isexpanded_id_modstandardelshdr' => "0",
+//            'mform_isexpanded_id_availabilityconditionsheader' => "0",
+//            'mform_isexpanded_id_activitycompletionheader' => "0",
+//            'mform_isexpanded_id_tagshdr' => "0",
+//            'mform_isexpanded_id_competenciessection' => "0",
+//            'name' => "XDEBUG TEST",
+//            'introeditor' => [
+//                'text' => '<p dir=>"ltr" style=>"text-align: left;">XDEBUG TEST<br></p>',
+//                'format' => "1",
+//                'itemid' => "399597115",
+//            ],
+//            'showdescription' => "0",
+//            'page' => [
+//                'text' => '<p dir=>"ltr" style=>"text-align: left;">XDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TESTXDEBUG TEST<br></p>',
+//                'format' => "1",
+//                'itemid' => "798293012",
+//            ],
+//            'printheading' => "1",
+//            'printintro' => "0",
+//            'printlastmodified' => "1",
+//            'visible' => "1",
+//            'cmidnumber' => "",
+//            'availabilityconditionsjson' => '{"op":"&","c":[],"showc":[]}',
+//            'completion' => "1",
+//            'tags' => "_qf__force_multiselect_submission",
+//            'competencies' => "_qf__force_multiselect_submission",
+//            'competency_rule' => "0",
+//            'submitbutton' => "Save and display"
+//        ];
+
+        $fromform = $mform->get_data();
+
+        /* TEST AREA END */
+
+//        return ['value'=> var_dump(get_class_methods($mform))];
+        return ['value'=> var_dump($fromform)];
     }
 
     /**
