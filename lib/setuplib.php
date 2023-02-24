5e3c2598a28e99601cec1c5bfa777ec2e76811ea
dhea farizka <farizkadhea@gmail.com>
2023-02-24 10:48:05 +0700

    clean comment debug

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
+//echo "<pre>", print_r(parse_url($CFG->wwwroot));die();
 /**
  * These functions are required very early in the Moodle
  * setup process, before any of the main libraries are
